ad29bc3747c7306c73ad9cd9912c8fae08243087
Sophie von Fromm <sfromm@bgc-jena.mpg.de>
2022-04-08 14:46:39 +0200

    df deleted

---
+++ Code/DataPreparation.R
@@ -0,0 +1,4 @@
+## Sophie von Fromm
+## 2022-April-08
+
+## Data preparation
---
+++ Code/MeanC_age.R
@@ -0,0 +1,11 @@
+## Sophie von Fromm
+## 2022-April-08
+
+##Calculate mean C age
+
+#Code adopted from Sue (Khomo et al. 2017, SI p. 19) and Jeff
+#Idea for code from Sierra et al. 2014 and radiocarbon book (chapter 3)
+
+##Load SoilR
+devtools::install_github("MPIBGC-TEE/SoilR-exp/pkg",
+                         upgrade = FALSE)
