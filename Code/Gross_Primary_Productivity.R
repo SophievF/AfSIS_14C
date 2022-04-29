0d323da125d8be0d0d2a1487873778862864b9f8
Sophie von Fromm <sfromm@bgc.mpg.de>
2022-04-29 18:44:32 +0300

    code to extract GPP

---
+++ Code/Gross_Primary_Productivity.R
@@ -0,0 +1,7 @@
+## Gross primary productivity (GPP) data for the AfSIS data ##
+## Sophie von Fromm ##
+## 2022-04-27 ##
+
+# GPP data was extracted based on long/lat from the Fluxcom network
+# FLUXCOM_RS_V006	Gross Primary Productivity in	gC m-2 d-1
+
